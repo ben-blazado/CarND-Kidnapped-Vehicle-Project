0be5c2bd2d9325b3eb5998514673f094eca5ea6e
ben-blazado <26291620+ben-blazado@users.noreply.github.com>
2021-04-23 20:20:22 -0600

    code works. finalize readme next.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -22,19 +22,14 @@ using std::string;
 using std::vector;
 
 using std::normal_distribution;
-std::default_random_engine gen;
+std::default_random_engine Gen;
 
-int X = 0;
-int Y = 1;
-int THETA = 2;
-
-double from_dist (double m, double std) {
-  
-  normal_distribution<double> dist(m, std);
-  
-  return dist(gen);
-}
-  
+/**
+ * Indices into std dev arrays
+ */
+#define X     0
+#define Y     1
+#define THETA 2
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
@@ -57,29 +52,39 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
   for (int i = 0; i < particles.size(); i++) {
     Particle& p = particles[i];
     p.id       = i;
-    p.x        = dist_x(gen);
-    p.y        = dist_y(gen);
-    p.theta    = dist_theta(gen);
+    p.x        = dist_x(Gen);
+    p.y        = dist_y(Gen);
+    p.theta    = dist_theta(Gen);
     p.weight   = 1.0;
     weights[i] = p.weight;
   }
 
+  is_initialized = true;
 }
 
-void set_pred (double pred_x, double pred_y, double pred_theta, 
+/**
+ * setState sets the state of particle p given predicted state values
+ *   and corresponding std devs; used in predictionYawZero and 
+ *   predictionYawNonZero.
+ */
+void setState (double pred_x, double pred_y, double pred_theta, 
     double std_pos[], Particle& p) {
   
-  normal_distribution<double> dist_x(pred_x, std_pos[X]);
-  normal_distribution<double> dist_y(pred_y, std_pos[Y]);
+  normal_distribution<double> dist_x    (pred_x, std_pos[X]);
+  normal_distribution<double> dist_y    (pred_y, std_pos[Y]);
   normal_distribution<double> dist_theta(pred_theta, std_pos[THETA]);
   
-  p.x     = dist_x(gen);
-  p.y     = dist_y(gen);
-  p.theta = dist_theta(gen);
+  p.x     = dist_x(Gen);
+  p.y     = dist_y(Gen);
+  p.theta = dist_theta(Gen);
 }
 
-
-void prediction_yaw_zero(double delta_t, double std_pos[], double velocity, 
+/**
+ * predictionYawZero Predicts the state for the next time step
+ *   using the process model without yaw; used in 
+ *   ParticleFilter::prediction.
+ */
+void predictionYawZero(double delta_t, double std_pos[], double velocity, 
     std::vector<Particle>& particles) {
           
   double dist = velocity * delta_t;
@@ -88,15 +93,19 @@ void prediction_yaw_zero(double delta_t, double std_pos[], double velocity,
 
     Particle& p = particles[i];
     
-    double pred_x     = p.x + dist * cos(p.theta);
-    double pred_y     = p.y + dist * sin(p.theta);
+    double pred_x = p.x + dist * cos(p.theta);
+    double pred_y = p.y + dist * sin(p.theta);
     
-    set_pred(pred_x, pred_y, p.theta, std_pos, p);
-    
+    setState(pred_x, pred_y, p.theta, std_pos, p);
   }
 }
 
-void prediction_yaw_nonzero(double delta_t, double std_pos[], double velocity, 
+/**
+ * predictionYawZero Predicts the state for the next time step
+ *   using the process model with yaw; used in
+ *   ParticleFilter::prediction.
+ */
+void predictionYawNonZero(double delta_t, double std_pos[], double velocity, 
       double yaw_rate, std::vector<Particle>& particles)  {
         
   double v_per_yaw_rate = velocity / yaw_rate;
@@ -109,7 +118,7 @@ void prediction_yaw_nonzero(double delta_t, double std_pos[], double velocity,
     double pred_x = p.x + v_per_yaw_rate * (sin(pred_theta) - sin(p.theta));
     double pred_y = p.y + v_per_yaw_rate * (cos(p.theta) - cos(pred_theta));
     
-    set_pred(pred_x, pred_y, pred_theta, std_pos, p);
+    setState(pred_x, pred_y, pred_theta, std_pos, p);
   }
 }
 
@@ -127,10 +136,10 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
   void (*pred_component) (double velocity, double theta, double pred_theta,
         double& x_comp, double& y_comp);
         
-  if (yaw_rate == 0) 
-    prediction_yaw_zero(delta_t, std_pos, velocity, particles);
+  if (yaw_rate == 0)
+    predictionYawZero(delta_t, std_pos, velocity, particles);
   else
-    prediction_yaw_nonzero(delta_t, std_pos, velocity, yaw_rate, particles);
+    predictionYawNonZero(delta_t, std_pos, velocity, yaw_rate, particles);
 }
 
 void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted, 
@@ -144,18 +153,77 @@ void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted,
    *   during the updateWeights phase.
    */
   for (int i = 0; i < observations.size(); i ++) {
+    
     LandmarkObs& o = observations[i];
     double lowest = INFINITY;
-    double closest = 0;
+    int id_closest;
+    
     for (int j = 0; j < predicted.size(); j ++) {
+      
       LandmarkObs& p = predicted[j];
       double d = dist(o.x, o.y, p.x, p.y);
       if (d < lowest) {
-        closest = j;
+        id_closest = j;
         lowest = d;
       }
-    o.id = predicted[closest].id;
     }
+    
+    // save index of closest landmark to id field of observation
+    // the closest landmark to an obsevation o is now found by:
+    // closest_landmark = landmark[o.id]
+    o.id = id_closest;
+  }
+}
+
+/**
+ * selectInRange saves landmarks from map_landmarks that
+ *   are in sensor_range of Particle p to predicted_landmarks;
+ *   used in ParticleFilter::updateWeights.
+ */
+void selectInRange(const Particle& p, double sensor_range, 
+                   const Map &map_landmarks, 
+                   vector<LandmarkObs> &predicted_landmarks) {
+  
+  for (int i = 0; i < map_landmarks.landmark_list.size(); i ++) {
+    
+    Map::single_landmark_s l = map_landmarks.landmark_list[i];
+    double d = dist(p.x, p.y, l.x_f, l.y_f);
+    
+    if (d <= sensor_range) {
+      // landmark l is in sensor range of particle p
+      LandmarkObs landmark_inrange;
+      landmark_inrange.id = l.id_i;
+      landmark_inrange.x  = l.x_f;
+      landmark_inrange.y  = l.y_f;
+      predicted_landmarks.push_back(landmark_inrange);
+    }
+  }
+  
+}
+
+/**
+ * transformToMap transforms observations from coordinates
+ *   relative to Particle p to map coordinates in map_observations;
+ *   used in ParticleFilter::updateWeights.
+ */
+void transformToMap(const Particle &p, 
+                    const vector<LandmarkObs> &observations, 
+                    vector<LandmarkObs> &map_observations) {
+
+  for (int i = 0; i < observations.size(); i ++) {
+    
+    LandmarkObs o = observations[i];
+    
+    double cos_theta = cos(p.theta);
+    double sin_theta = sin(p.theta);
+    double x_map = p.x + o.x*cos_theta - o.y*sin_theta;
+    double y_map = p.y + o.x*sin_theta + o.y*cos_theta;
+    
+    LandmarkObs map_observation;
+    map_observation.x  = x_map;
+    map_observation.y  = y_map;
+    map_observations.push_back(map_observation);
+    
   }
 }
 
@@ -175,56 +243,59 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
    *   and the following is a good resource for the actual equation to implement
    *   (look at equation 3.33) http://planning.cs.uiuc.edu/node99.html
    */
+   
+  double std_x = std_landmark[0];
+  double std_y = std_landmark[1];
+  double normalizer = 2 * M_PI * std_x * std_y;
+   
   for (int i = 0; i < particles.size(); i ++) {
+    
     Particle& p = particles[i];
     
-    // generate predicted_landmarks in range of particle p
-    vector<LandmarkObs> predicted_landmarks;
-    for (int j = 0; j < map_landmarks.landmark_list.size(); j ++) {
-      Map::single_landmark_s l = map_landmarks.landmark_list[j];
-      double d = dist(p.x, p.y, l.x_f, l.y_f);
-      if (d <= sensor_range) {
-        LandmarkObs landmark_inrange;
-        landmark_inrange.id = l.id_i;
-        landmark_inrange.x = l.x_f;
-        landmark_inrange.y = l.y_f;
-        predicted_landmarks.push_back(landmark_inrange);
-      }
-    }
-
+    // select landmarks in sensor range of particle p
+    vector<LandmarkObs> map_predicted_landmarks;
+    selectInRange(p, sensor_range, map_landmarks, map_predicted_landmarks);
+  
     // transform observed landmarks from vehicle to map coordinates
     vector<LandmarkObs> map_observations;
-    for (int j = 0; j < observations.size(); j ++) {
-      LandmarkObs o = observations[j];
-      
-      double cos_theta = cos(p.theta);
-      double sin_theta = sin(p.theta);
-      double x_map = p.x + o.x*cos_theta - o.y*sin_theta;
-      double y_map = p.y + o.x*sin_theta - o.y*cos_theta;
-      
-      LandmarkObs map_observation;
-      map_observation.x  = x_map;
-      map_observation.y  = y_map;
-      map_observations.push_back(map_observation);
-    }
+    transformToMap(p, observations, map_observations);
     
     // associate nearest predicted landmark to observed landmark
-    dataAssociation (predicted_landmarks, map_observations);
+    dataAssociation(map_predicted_landmarks, map_observations);
     
+    // set up variables to record landmarks associated with particle p
+    vector<int> associations;   // landmark id associated with particle p
+    vector<double> sense_x;     // observed x coordinate of landmark
+    vector<double> sense_y;     // observed y coordinate of landmark
+
     // calculate particle weight
-    double std_x = std_landmark[0];
-    double std_y = std_landmark[1];
-    double normalizer = 2 * M_PI * std_x * std_y;
-    for (int j = 0; j < observations.size(); j ++) {
-      LandmarkObs o = observations[j];
-      LandmarkObs closest_landmark = predicted_landmarks[o.id];
+    p.weight = 1.0;
+    
+    for (int j = 0; j < map_observations.size(); j ++) {
       
-      double dx = (o.x - closest_landmark.x);
-      double dy = (o.y - closest_landmark.y);
+      LandmarkObs map_o = map_observations[j];
+      // map_o.id is the id of the closest predicted_landmark
+      LandmarkObs map_closest = map_predicted_landmarks[map_o.id];
+      
+      // calculate weight of particle using mult-variate gaussian
+      double dx = (map_o.x - map_closest.x);
+      double dy = (map_o.y - map_closest.y);
       double e = exp(-((dx*dx)/(2*std_x*std_x) + (dy*dy)/(2*std_y*std_y)));
       
-      p.weight *= e / normalizer;
+      p.weight *= (e / normalizer);
+      
+      // record closest landmark as associated with particle p
+      associations.push_back(map_closest.id);  // save id of landmark
+      sense_x.push_back(map_o.x);              // save observed x coordinate
+      sense_y.push_back(map_o.y);              // save observed y coordinate
     }
+    
+    SetAssociations(p, associations, sense_x, sense_y);
+    
+    // vector of weights will be used in 
+    // discrete_distribution in ParticleFilter::resample
+    weights[i] = p.weight;
+    
   }
 }
 
@@ -236,6 +307,26 @@ void ParticleFilter::resample() {
    *   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
    */
 
+  // setup discrete distribution using the calculated weights
+  std::discrete_distribution<int> dist(weights.begin(), weights.end());
+  
+  // vector of particles selected from sample
+  std::vector<Particle> selected;
+  
+  // select particles from sample according to discrete_distribution dist
+  // this is the part the filters out particles
+  for (int n = 0; n < particles.size(); n ++) {
+    
+    int i = dist(Gen);
+    selected.push_back(particles[i]);
+  }
+  
+  // clear out existing set of particles
+  particles.clear();
+  
+  // save selected to particles for use in next iteration
+  particles.assign(selected.begin(), selected.end());
+  
 }
 
 void ParticleFilter::SetAssociations(Particle& particle, 
